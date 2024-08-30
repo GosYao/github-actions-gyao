a2cbbcb0f7f4b59d6957de7fd9b67055bd55cbdb
DC-PNW <78760327+DC-PNW@users.noreply.github.com>
2024-08-30 10:28:49 -0700

    code change

--- app/app.js
+++ app/app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Wiz Code Demo!</h1><p>This is a simple Node.js app served with Docker.</p>');
+  res.send('<h1>Wiz Code Demo!</h1><p>This is a simple Node.js app served with Docker</p>');
 });
 
 app.listen(port, () => {
